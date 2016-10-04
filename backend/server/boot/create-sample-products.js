ccf9f9a63270260eb818fe98f1dee00f3a0f9868
Tomáš Horáček <tomas.horacek@gmail.com>
2016-10-04 14:03:04 +0200

    Basic backend skeleton

---
+++ backend/server/boot/create-sample-products.js
@@ -0,0 +1,26 @@
+module.exports = function(app) {
+  const { EshopProduct } = app.models;
+  if (!EshopProduct) { return; }
+
+  EshopProduct.create([
+      {
+        title: 'Škoda Superb',
+        price: 750000,
+        shortInfo: 'Luxury car produced in the Czech Republic.',
+      },
+      {
+        title: 'Ford Focus',
+        price: 600000,
+        shortInfo: 'Sports car made in USA.',
+      },
+      {
+        title: 'Moped',
+        price: 1000,
+        shortInfo: 'No comment.',
+      },
+  ], function(err, products) {
+    if (err) throw err;
+
+    console.log('Models created: \n', products);
+  });
+};
