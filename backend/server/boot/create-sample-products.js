eac9d500ba72aff522cd8294c84bd336738abf44
Perdedor <mike.hric@gmail.com>
2016-10-11 17:42:01 +0200

    a

--- backend/server/boot/create-sample-products.js
+++ backend/server/boot/create-sample-products.js
@@ -1,26 +1,28 @@
 module.exports = function(app) {
-  const { EshopProduct } = app.models;
-  if (!EshopProduct) { return; }
+  app.dataSources.mysqlds.autoupdate('EshopProduct', function(err) {
+    const { EshopProduct } = app.models;
+    if (!EshopProduct) { return; }
 
-  EshopProduct.create([
-      {
-        title: 'Škoda Superb',
-        price: 750000,
-        shortInfo: 'Luxury car produced in the Czech Republic.',
-      },
-      {
-        title: 'Ford Focus',
-        price: 600000,
-        shortInfo: 'Sports car made in USA.',
-      },
-      {
-        title: 'Moped',
-        price: 1000,
-        shortInfo: 'No comment.',
-      },
-  ], function(err, products) {
-    if (err) throw err;
+    EshopProduct.create([
+        {
+          title: 'Škoda Superb',
+          price: 750000,
+          shortInfo: 'Luxury car produced in the Czech Republic.',
+        },
+        {
+          title: 'Ford Focus',
+          price: 600000,
+          shortInfo: 'Sports car made in USA.',
+        },
+        {
+          title: 'Moped',
+          price: 1000,
+          shortInfo: 'No comment.',
+        },
+    ], function(err, products) {
+      if (err) throw err;
 
-    console.log('Models created: \n', products);
+      console.log('Models created: \n', products);
+    });
   });
 };
