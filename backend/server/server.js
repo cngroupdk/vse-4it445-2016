d3d39b95ada2c475757fb2226b9477ee8ba32aa1
Tomas Horacek <tomas.horacek@gmail.com>
2016-10-11 12:25:30 +0200

    Changes after 3rd practice (class 16.15-17.45)

--- backend/server/server.js
+++ backend/server/server.js
@@ -17,6 +17,15 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
+app.get('/hello', function (req, res) {
+  const data = {
+    hello: "World",
+    time: new Date(),
+  };
+  res.header({ 'Content-Type': 'application/json' });
+  res.send(JSON.stringify(data));
+});
+
 app.start = function() {
   // start the web server
   return app.listen(function() {
--- frontend/src/pages/ProductsPage.js
+++ frontend/src/pages/ProductsPage.js
@@ -1,34 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import { ProductList } from '../components/ProductList/ProductList.js';
 
 export class ProductsPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [] };
+  }
+
+  componentDidMount() {
+    axios('http://dev.backend.horato01.vse.handson.pro/api/products')
+      .then((response) => {
+        this.setState({
+          products: response.data
+        });
+      });
+  }
+
+  addNewProduct() {
+
+  }
+
   render() {
-    const products = [
-      {
-        id: 1,
-        title: 'Škoda Superb',
-        price: '750 000',
-        shortInfo: 'Luxury car produced in the Czech Republic.',
-      },
-      {
-        id: 2,
-        title: 'Ford Focus',
-        price: '600 000',
-        shortInfo: 'Sports car made in USA.',
-      },
-      {
-        id: 3,
-        title: 'Moped',
-        price: '1 000',
-        shortInfo: 'No comment.',
-      },
-    ]
+    const { products } = this.state;
+
     return (
       <div>
         <div className="jumbotron">
           <h1>All Products</h1>
         </div>
+        <button onClick={this.addNewProduct.bind(this)}>
+          click me
+        </button>
         <ProductList products={products}/>
       </div>
     );
