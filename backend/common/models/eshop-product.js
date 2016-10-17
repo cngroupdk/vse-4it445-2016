444dc2daf530ff6feabe5bbb6476fa4fefebf459
Tomas Horacek <tomas.horacek@gmail.com>
2016-10-18 00:12:31 +0200

    Changes after 4th practice (class 18.00-19.30)

--- backend/common/models/eshop-product.js
+++ backend/common/models/eshop-product.js
@@ -1,5 +1,26 @@
 'use strict';
 
-module.exports = function(Eshopproduct) {
+module.exports = function(EshopProduct) {
+  EshopProduct.withCategory = function(id, msg2, callback) {
 
+    EshopProduct.find({ include: 'category', where: { id }}, function(err, products) {
+      if (err) { return callback(err); }
+
+      callback(null, products);
+    })
+  }
+
+  EshopProduct.remoteMethod(
+      'withCategory',
+      {
+        accepts: [
+          {arg: 'msg1', type: 'id'},
+          {arg: 'msg2', type: 'string'},
+        ],
+        http: {
+          verb: 'get',
+        },
+        returns: {arg: 'products', type: '[EshopProduct]', root: true},
+      }
+  );
 };
--- backend/server/boot/create-sample-products.js
+++ backend/server/boot/create-sample-products.js
@@ -1,26 +1,51 @@
 module.exports = function(app) {
-  const { EshopProduct } = app.models;
-  if (!EshopProduct) { return; }
+  app.dataSources.mysqlds.autoupdate('EshopCategory', function(err) {
+    const { EshopCategory } = app.models;
+    if (!EshopCategory) { return; }
 
-  EshopProduct.create([
-      {
-        title: 'Škoda Superb',
-        price: 750000,
-        shortInfo: 'Luxury car produced in the Czech Republic.',
-      },
-      {
-        title: 'Ford Focus',
-        price: 600000,
-        shortInfo: 'Sports car made in USA.',
-      },
-      {
-        title: 'Moped',
-        price: 1000,
-        shortInfo: 'No comment.',
-      },
-  ], function(err, products) {
-    if (err) throw err;
+    EshopCategory.count({}, function(err, count) {
 
-    console.log('Models created: \n', products);
+      if (count === 0) {
+        EshopCategory.create([
+          {
+            name: 'Car',
+          },
+          {
+            name: 'Bike',
+          }
+        ], function(err, categories) {
+          if (err) throw err;
+
+          console.log('Models created: \n', categories);
+        });
+      }
+    })
+  });
+
+  app.dataSources.mysqlds.autoupdate('EshopProduct', function(err) {
+    const { EshopProduct } = app.models;
+    if (!EshopProduct) { return; }
+
+    EshopProduct.create([
+        {
+          title: 'Škoda Superb',
+          price: 750000,
+          shortInfo: 'Luxury car produced in the Czech Republic.',
+        },
+        {
+          title: 'Ford Focus',
+          price: 600000,
+          shortInfo: 'Sports car made in USA.',
+        },
+        {
+          title: 'Moped',
+          price: 1000,
+          shortInfo: 'No comment.',
+        },
+    ], function(err, products) {
+      if (err) throw err;
+
+      console.log('Models created: \n', products);
+    });
   });
 };
