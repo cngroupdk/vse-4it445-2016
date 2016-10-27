d6aaaf4710ed7a824bc5fc44e1de7c16993f48a1
Tomas Horacek <tomas.horacek@gmail.com>
2016-10-27 11:52:52 +0200

    Changes after 6th practice (class 18.00-19.30)

--- frontend/src/pages/ShoppingCartCheckoutPage.js
+++ frontend/src/pages/ShoppingCartCheckoutPage.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export class ShoppingCartCheckoutPage extends Component {
+import api from '../api.js';
+
+export class ShoppingCartCheckoutPageRaw extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      firstName: "",
+    };
 
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(event) {
+    event.preventDefault();
+
+    const { items } =  this.props;
+    const formItems = items.map(({ product, quantity }) => ({
+      productId: product.id,
+      quantity,
+    }))
+
+    const formData = new FormData(event.target);
+    formData.append(
+      'items',
+      JSON.stringify(formItems)
+    );
+
+    api.post('orders/submit', formData)
+      .then(({ data }) => console.log('data', data));
   }
 
   render() {
@@ -18,12 +41,27 @@ export class ShoppingCartCheckoutPage extends Component {
         </div>
         <form onSubmit={this.handleSubmit}>
           <div>
-            <div><label htmlFor="firstName">First Name</label></div>
-            <input type="text" name="firstName" id="firstName" />
+            <div>
+              <label className="some-class" htmlFor="firstName">First Name</label>
+            </div>
+            <input
+              value={this.state.firstName}
+              onChange={event => this.setState({
+                firstName: event.target.value.slice(0, 5),
+              })}
+              type="text"
+              name="firstName"
+              id="firstName"
+            />
           </div>
           <div>
             <div><label htmlFor="lastName">Last Name</label></div>
-            <input type="text" name="lastName" id="lastName" />
+            <input
+              defaultValue="Doe"
+              type="text"
+              name="lastName"
+              id="lastName"
+            />
           </div>
           <div>
             <div><label htmlFor="address">Address</label></div>
@@ -37,3 +75,16 @@ export class ShoppingCartCheckoutPage extends Component {
     );
   }
 }
+
+import { getItems } from '../components/ShoppingCart/reducer.js';
+
+function mapStateToProps(state) {
+  const { shoppingCart } = state;
+  return {
+    items: getItems(shoppingCart),
+  };
+}
+
+export const ShoppingCartCheckoutPage = connect(
+  mapStateToProps,
+)(ShoppingCartCheckoutPageRaw);
