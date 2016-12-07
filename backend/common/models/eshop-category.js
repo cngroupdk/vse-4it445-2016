6af764d7177beae4106f3f8e0b14eb37c4504930
Tomáš Horáček <tomas.horacek@gmail.com>
2016-12-07 11:43:54 +0100

    Add createdAt with hook to EshopCategory

--- backend/common/models/eshop-category.js
+++ backend/common/models/eshop-category.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function(EshopCategory) {
+  EshopCategory.observe('before save', function limitToTenant(ctx, next) {
+    const { instance } = ctx;
+
+    if (instance) {
+      const { id } = instance;
+      if (!id) {
+        instance.createdAt = new Date();
+      }
+    }
+    
+    next();
+  });
 
 };
